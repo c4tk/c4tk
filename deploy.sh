9f83de7be8662fcdde77c5e8c2a7198253e46c44
Kamil Gałuszka <galuszkak@gmail.com>
2016-03-30 18:38:32 +0200

    change deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -3,7 +3,8 @@ set -e # exit with nonzero exit code if anything fails
 
 # clear and re-create the out directory
 rm -rf dist || exit 0;
-mkdir dist;
+MESSAGE=$(git log -1 HEAD --pretty=format:%s)
+git clone "https://${GH_TOKEN}@${GH_REF}" dist --branch gh-pages
 
 # run our compile script, discussed above
 grunt build
@@ -25,10 +26,10 @@ git config user.email "galuszkak@gmail.com"
 # The first and only commit to this new Git repo contains all the
 # files present with the commit message "Deploy to GitHub Pages".
 git add .
-git commit -m "Deploy to GitHub Pages"
+git commit -m "${MESSAGE}"
 
 # Force push from the current repo's master branch to the remote
 # repo's gh-pages branch. (All previous history on the gh-pages branch
 # will be lost, since we are overwriting it.) We redirect any output to
 # /dev/null to hide any sensitive credential data that might otherwise be exposed.
-git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" master:gh-pages > /dev/null 2>&1
+git push --force --quiet "https://${GH_TOKEN}@${GH_REF}" gh-pages:gh-pages > /dev/null 2>&1
